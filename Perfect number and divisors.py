ab02913c47b23b508ad877ab0ed19b92d9901a5e
SIMRAN SWAIN <61618523+SimranSwain@users.noreply.github.com>
2021-06-06 10:25:10 +0530

    Add files via upload

---
+++ Perfect number and divisors.py
@@ -0,0 +1,12 @@
+t=int(input())
+while(t>0):
+  sum=0
+  n=int(input())
+  for i in range(1,n):
+    if(n%i==0):
+      sum+=i
+  if(sum==n):
+    print("true")
+  else:
+    print("false")
+  t=t-1
