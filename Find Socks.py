bce9a73340b9e952f338149620fea718cbeaf995
SIMRAN SWAIN <61618523+SimranSwain@users.noreply.github.com>
2021-06-17 19:26:45 +0530

    Add files via upload

---
+++ Find Socks.py
@@ -0,0 +1,10 @@
+#PrepBuddy is getting late for college. She is looking for a matching pair of socks from a box
+# full of socks. In the worst-case scenario, how many socks should PrepBuddy remove from her drawer
+# until she finds a matching pair.
+
+t=int(input())
+while(t>0):
+  s=int(input())
+  p=s+1
+  print(p)
+  t=t-1
