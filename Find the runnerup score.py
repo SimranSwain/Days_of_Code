0b69c131ec863809e42d5e9cf5dfb48ae2b2ebbd
SIMRAN SWAIN <61618523+SimranSwain@users.noreply.github.com>
2021-06-22 00:16:02 +0530

    Add files via upload

---
+++ Find the runnerup score.py
@@ -0,0 +1,15 @@
+ n = int(input())
+    arr = list(map(int,input().split()))
+    
+    a=max(arr)
+    c=arr.count(a)
+    for i in range(c):
+        arr.remove(a)
+    
+    print(max(arr))
+    
+    
+    
+    
+    # arr.sort()
+    # print(arr[(arr.index(max(arr)))-1])
