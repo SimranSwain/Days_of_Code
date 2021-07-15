5fd94556790e7b2733e54b5b3dd4ae5f4982a54d
SIMRAN SWAIN <swainsimran4@gmail.com>
2021-07-15 16:17:12 +0530

    Add files via upload

---
+++ The Minion Game.py
@@ -0,0 +1,23 @@
+def minion_game(string):
+   
+    s =len(string)
+    vow,con=0,0
+    
+    for i in range(s):
+        if string[i] in 'AEIOU':
+            vow = vow+(s-i)
+        else:
+            con = con+(s-i)
+            
+    if con>vow:
+        print("Stuart {}" .format(con))
+    elif con==vow:
+        print("Draw")
+    else:
+        print("Kevin {}" .format(vow))
+        
+        
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
